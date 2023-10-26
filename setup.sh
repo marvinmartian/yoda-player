11fd329cc1dcafcf4a929f204c44ca746e366f66
Marvin <marvinmartian@gmail.com>
2023-10-26 12:38:48 -0600

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+
+
+sudo apt install python3-dev python3-pip
+sudo pip3 install spidev
+sudo pip3 install mfrc522
+
+
+
+git clone https://github.com/waveshare/WM8960-Audio-HAT
+cd WM8960-Audio-HAT
+sudo ./install.sh
+sudo reboot
+
