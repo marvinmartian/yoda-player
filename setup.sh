3394d28edc96c2e06bde79384ec36e7d91a456e6
Marvin <marvinmartian@gmail.com>
2023-11-21 10:53:14 -0700

    Add additional steps in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt install git python3-dev python3-venv python3-pip mpg123 jq -y
-sudo pip3 install spidev
-sudo pip3 install mfrc522
+sudo apt install git python3-dev python3-venv python3-pip mpg123 libsystemd-dev jq -y
+
+python3 -m venv venv
+source venv/bin/activate
+cd reader/
+pip3 install -r requirements.txt
+
+# sudo pip3 install spidev
+# sudo pip3 install mfrc522
+# sudo pip3 install systemd-python
 
 
 
