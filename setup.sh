39dcbf2fa2b6c2d6fc3ea341a0955815feed29e2
Marvin <marvinmartian@gmail.com>
2023-12-02 20:17:22 -0700

    Add download from the release page for player binary

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 sudo apt-get update
 sudo apt install git python3-dev python3-venv python3-pip mpg123 libsystemd-dev jq -y
 
+curl -L https://github.com/marvinmartian/yoda-player/releases/download/v0.0.1/go_player -o player/go_player
+
 python3 -m venv venv
 source venv/bin/activate
 cd reader/
